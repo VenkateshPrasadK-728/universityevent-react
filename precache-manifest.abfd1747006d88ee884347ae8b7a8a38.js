13d64f17ac3da9726665ede3da127abf382a8c8f
Venki <venkateshprasad728@gmail.com>
2020-06-11 22:49:21 +0530

    Updates

---
+++ precache-manifest.abfd1747006d88ee884347ae8b7a8a38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92f10b446c03ebf0ef3c",
+    "url": "/universityevent-react/static/css/main.039e01ca.chunk.css"
+  },
+  {
+    "revision": "92f10b446c03ebf0ef3c",
+    "url": "/universityevent-react/static/js/main.92f10b44.chunk.js"
+  },
+  {
+    "revision": "f977ea383b88177f8a34",
+    "url": "/universityevent-react/static/css/1.4f474294.chunk.css"
+  },
+  {
+    "revision": "f977ea383b88177f8a34",
+    "url": "/universityevent-react/static/js/1.f977ea38.chunk.js"
+  },
+  {
+    "revision": "1feb68ffd5668cce0742",
+    "url": "/universityevent-react/static/js/runtime~main.1feb68ff.js"
+  },
+  {
+    "revision": "32481187a0580c46066666b5aa336f1a",
+    "url": "/universityevent-react/index.html"
+  }
+];
